5cf66c5dc228abc06cb7014b8d2aea436d1ae889
Gabriela de Queiroz <gabidequeiroz@gmail.com>
2018-07-28 19:25:56 -0700

    Add checks to the function

--- R/script-to-update-current-chapters.R
+++ R/script-to-update-current-chapters.R
@@ -13,7 +13,7 @@
 # 'prospect' - is in the planning stage is not set up on meetup.com yet.
 #-----------------------------------------------------------#
 
-
+library(futile.logger)
 library(data.table)
 data <- fread("Current-Chapters.csv")
 
@@ -21,9 +21,21 @@ data <- fread("Current-Chapters.csv")
 
 # add meetup url
 add_meetup_url <- function(data, country, city, meetup_url) {
-  data[(Country == country & City == city), 
-        `:=` (Meetup = meetup_url)]
-  data.table::fwrite(data, "Current-Chapters.csv")
+  # check if meetup_url is empty
+  before <- data[(Country == country & City == city), ]
+  if(before$Meetup %in% c("", NA)) {
+    futile.logger::flog.info("Before", before, capture = TRUE)
+    data[(Country == country & City == city), 
+         `:=` (Meetup = meetup_url)]
+    after <- data[(Country == country & City == city), ]
+    futile.logger::flog.info("After", after, capture = TRUE)
+    changed_column <- gsub(".*Column\\s*|:.*", "", all.equal(before, after))
+    clean <- gsub("'", "", changed_column)
+    futile.logger::flog.info("Change", after[, ..clean], capture = TRUE)
+    data.table::fwrite(data, "Current-Chapters.csv")
+  } else {
+    stop("This chapter has a already meetup url.")
+  }
 }
 
 # change status from Propective to Progress
@@ -32,7 +44,7 @@ change_status_to_progress <- function(data) {
        `:=` (Status = "Progress")]
   data.table::fwrite(data, "Current-Chapters.csv")
 }
-change_status_to_progress(data)
+
 
 
 
