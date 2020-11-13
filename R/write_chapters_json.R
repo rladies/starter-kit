bcb942b904de75a7417d510fd8e0fb1dc013f03c
Athanasia Monika Mowinckel <a.m.mowinckel@psykologi.uio.no>
2020-11-13 23:55:47 +0100

    make as script not func

--- R/write_chapters_json.R
+++ R/write_chapters_json.R
@@ -1,17 +1,17 @@
-write_chapters <- function(){
-  data <- read.csv("Current-Chapters.csv", header = TRUE)
-  names(data) <- tolower(names(data))
-  
-  data <- unique(data)
-  
-  data <- dplyr::arrange(data, country, city)
-  
-  # replace empty cells with NA
-  data[data == ""] <- NA
-  
-  data <- dplyr::nest_by(data, country, .key = "chapters")
-  
-  jsonlite::write_json(data, 
-                       "chapters.json", 
-                       pretty = TRUE)
-}
+
+data <- read.csv("Current-Chapters.csv", header = TRUE)
+names(data) <- tolower(names(data))
+
+data <- unique(data)
+
+data <- dplyr::arrange(data, country, city)
+
+# replace empty cells with NA
+data[data == ""] <- NA
+
+data <- dplyr::nest_by(data, country, .key = "chapters")
+
+jsonlite::write_json(data, 
+                     "chapters.json", 
+                     pretty = TRUE)
+
