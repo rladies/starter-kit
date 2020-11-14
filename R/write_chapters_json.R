fbea3c2af239b71a397fbaf162d4b659218cfc1d
Athanasia Monika Mowinckel <a.m.mowinckel@psykologi.uio.no>
2020-11-14 17:33:42 +0100

    tidy urls and reduce data

--- R/write_chapters_json.R
+++ R/write_chapters_json.R
@@ -8,8 +8,22 @@ data <- dplyr::arrange(data, country, city)
 data$organizers <- gsub("\n", " ", data$organizers )
 data$organizers <- strsplit(data$organizers, ", ")
 
+# remove urls for accounts
+data$twitter <- gsub("https://twitter.com/|/", "", data$twitter)
+data$instagram <- gsub("https://www.instagram.com/|/", "", data$instagram)
+data$facebook <- gsub("https://www.facebook.com/|/", "", data$facebook)
+data$periscope <- gsub("https://www.periscope.tv/|/", "", data$periscope)
+data$youtube <- gsub("https://www.youtube.com/|/", "", data$youtube)
+data$github <- gsub("https://github.com/rladies/|/", "", data$github)
+
 # replace empty cells with NA
 data[data == ""] <- NA
+
+# place socials in a nested list
+data <- dplyr::nest_by(data, country, state.region, 
+                city, meetup, organizers,
+                email, status, slack, 
+                .key = "socials")
 
 data <- dplyr::nest_by(data, country, .key = "chapters")
 
